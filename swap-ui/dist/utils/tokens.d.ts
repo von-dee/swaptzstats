179ef87ac66ab3c5e849d7a70f6bf0d3ab14ac42
von-dee <vondeesela@gmail.com>
2022-04-18 17:40:44 +0000

    done

--- swap-ui/dist/utils/tokens.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { AccountInfo as TokenAccount } from "@solana/spl-token";
-import { Connection, PublicKey } from "@solana/web3.js";
-export declare function getOwnedAssociatedTokenAccounts(connection: Connection, publicKey: PublicKey): Promise<(PublicKey | {
-    publicKey: any;
-    account: TokenAccount;
-})[]>;
-export declare function parseTokenAccountData(data: Buffer): TokenAccount;
